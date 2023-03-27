795763a9d778345fc2ed13b60fcc51109d30d2ec
Dennis Kuijs <d.kuijs@student.fontys.nl>
2023-03-27 11:08:14 +0200

    added caching

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,23 @@
+const cacheName="SAMPLE_SITE_V1";
+//Just take into account that the "files" below are request-url's and not filenames perse. So for your root of your website yous should include "./" and if you use my site (or another plain HTML-site) also "index.html". If you use a server-side language and have friendly url's that could be something like "news/this-is-a-newsarticle/".
+const appFiles=[
+	"./manifest.webmanifest",
+	"./js/scripts.js",
+	"./css/stylesheet.css",
+	"./images/yes.gif", 
+	"./",
+  "./index.html"
+];
+
+
 self.addEventListener("install",(installing)=>{
     console.log("Service Worker: I am being installed, hello world!");
+    installing.waitUntil(
+      caches.open("SAMPLE_SITE_V1").then((cache)=>{
+        console.log("Service Worker: Caching important offline files");
+        return cache.addAll("appFiles");
+      })
+    );
   });
   
   self.addEventListener("activate",(activating)=>{	
