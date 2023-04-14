8c8a8fca366da75d472403ea6abde14dcff772b7
Dennis Kuijs <d.kuijs@student.fontys.nl>
2023-04-14 13:48:50 +0200

    added part 3

--- service-worker.js
+++ service-worker.js
@@ -33,7 +33,7 @@ self.addEventListener("install",(installing)=>{
           console.log("Service Worker: Resource "+fetching.request.url+" not available in cache");
           return caches.open(cacheName).then((cache)=>{
             console.log("Service Worker: Caching (new) resource "+fetching.request.url);
-            // OUTCOMMENT THIS LINE FOR ADDING DOWNLOADED RESOURCES TO YOUR CACHE: cache.put(fetching.request,response.clone());
+            cache.put(fetching.request,response.clone());
             return response;
           });
         }).catch(function(){      
@@ -45,6 +45,16 @@ self.addEventListener("install",(installing)=>{
   });
   
   self.addEventListener("push",(pushing)=>{
-      console.log("Service Worker: I received some push data, but because I am still very simple I don't know what to do with it :(");
+    if(pushing.data){
+      pushdata=JSON.parse(pushing.data.text());		
+      console.log("Service Worker: I received this:",pushdata);
+      if((pushdata["title"]!="")&&(pushdata["message"]!="")){			
+        const options={ body:pushdata["message"] }
+        self.registration.showNotification(pushdata["title"],options);
+        console.log("Service Worker: I made a notification for the user");
+      } else {
+        console.log("Service Worker: I didn't make a notification for the user, not all the info was there :(");			
+      }
+    }    
   })
   
